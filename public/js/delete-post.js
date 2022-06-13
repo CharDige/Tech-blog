46d4eaa84ab4d7db8bc6633a769bc925caec2337
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-06-13 17:31:55 +0930

    Created DELETE route to delete blog posts, including an event listener on the button

---
+++ public/js/delete-post.js
@@ -0,0 +1,17 @@
+const deleteHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete blog post');
+        }
+    }
+};
+
+document.querySelector('.dashboard-post').addEventListener('click', deleteHandler);
