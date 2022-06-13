daacc449f4a73b3d01adc0017e89fec1cb6af4bf
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-06-13 15:13:51 +0930

    Added GET route to Dashboard page, including authentication requirements and redirects

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -40,6 +40,24 @@ router.get('/post/:id', async (req, res) => {
         res.render('post', {
             ...post,
             logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
         });
     } catch (err) {
         res.status(500).json(err);
