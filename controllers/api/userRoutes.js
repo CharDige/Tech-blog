9c332d4cb9e9b618f9d769786d009d53a6e47aa7
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-06-13 14:42:00 +0930

    Added POST route for users logging into the web application

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,3 +15,42 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        // Find if email address is already in database
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        // If email doesn't exist in database, send message saying the email or password is incorrect
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password. Please try again" });
+            return;
+        }
+
+        // Check if the password matches what's in the database
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        // If password doesn't match what's in the database, send message saying the email or password is incorrect
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect email or password. Please try again" });
+            return;
+        }
+
+        // If all userData is correct and matches what's in the database, save the session and log the user in
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({
+                user: userData,
+                message: "You are now logged in!" 
+            });
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
